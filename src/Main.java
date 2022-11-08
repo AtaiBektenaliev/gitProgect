f65d16d3b4e9f92e2eff777fec6ca4902ef57049
Atai <abektenaliev@inbox.ru>
2022-11-08 20:07:56 +0600

    new commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         Person person = new Person("Sasha", 18, "Moscow");
 
         System.out.println(person);
+        System.out.println("Это новая ветка с моим именем");
     }
 }
